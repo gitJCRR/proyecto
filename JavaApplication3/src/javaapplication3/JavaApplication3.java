5479b39299f75673ca6103291a2dcde6b484db1a
Juan Carlos <Juan Carlos@DESKTOP-BKOTJMN.wifi.educarm.net>
2017-02-23 09:46:26 +0100

    hola

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -15,7 +15,7 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hola");
+        System.out.print("Hola. Esto es un cambio");
     }
     
 }
